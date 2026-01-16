e0736a7f1a5dbc5e0926e2e4e8c4e6dc3ec0cd3c
Mouad TAJ <mouadtaj.etudes@gmail.com>
2026-01-16 16:24:57 +0100

    Feat(Data): -Ajout des models dans populatedatabasecommand -Ajout du faker (dans composer json et utilisation dans PopulateDatabaseCommand -check in TODO action

--- TODO.md
+++ TODO.md
@@ -5,8 +5,8 @@
 Comme pour la semaine dernière, utiliser les principes gitflow pour ce projet, nommer correctement vos branches et vos commits ([conventional commits](https://www.conventionalcommits.org/en/v1.0.0/), [gitmoji](https://gitmoji.dev/) ou autre).
 
 ### Objectif
-- [ ] Installer et lancer le projet
-- [ ] Modifier `PopulateDatabaseCommand.php` pour ajouter des données aléatoire cohérentes (2-4 sociétés avec 2-3 bureaux et une dizaine d'employés) 
+- [x] Installer et lancer le projet
+- [x] Modifier `PopulateDatabaseCommand.php` pour ajouter des données aléatoire cohérentes (2-4 sociétés avec 2-3 bureaux et une dizaine d'employés) 
 - [ ] Ajouter Phpcs et Phpstan au projet
 - [ ] Ajouter ESLint au projet
 - [ ] Deplacer le js et css dans un dossiers `assets` à la racine du projet, et les build dans un dossier `./public/build` grâce à vite
--- src/Console/PopulateDatabaseCommand.php
+++ src/Console/PopulateDatabaseCommand.php
@@ -34,38 +34,80 @@ class PopulateDatabaseCommand extends Command
         /** @var \Illuminate\Database\Capsule\Manager $db */
         $db = $this->app->getContainer()->get('db');
 
+        $faker = \Faker\Factory::create('fr_FR');
+
         $db->getConnection()->statement("SET FOREIGN_KEY_CHECKS=0");
         $db->getConnection()->statement("TRUNCATE `employees`");
         $db->getConnection()->statement("TRUNCATE `offices`");
         $db->getConnection()->statement("TRUNCATE `companies`");
-        $db->getConnection()->statement("SET FOREIGN_KEY_CHECKS=1");
+        // $db->getConnection()->statement("SET FOREIGN_KEY_CHECKS=1");
+
+        for($i=1;$i<4;$i++) {
+            $compname=$faker->company;
+            Company::create([
+                'id' => $i,
+                'name' => $compname,
+                'phone' => $faker->phoneNumber,
+                'email' => $faker->companyEmail,
+                'website' => $faker->url,
+                'image' => "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Google_office_%284135991953%29.jpg/800px-Google_office_%284135991953%29.jpg?20190722090506",
+                'head_office_id' => $i,
+            ]);
+               for($j=1;$j<4;$j++) {
+            $office = Office::create([
+                'name' => $compname . ' Office ' . $j,
+                'address' => $faker->streetAddress,
+                'city' => $faker->city,
+                'zip_code' => $faker->postcode,
+                'country' => $faker->country,
+                'email' => $faker->companyEmail,
+                'phone' => $faker->phoneNumber,
+                'company_id' => $i,
+            ]);
+
+            for($k=1;$k<10;$k++) {
+            Employee::create([
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+                'email' => $faker->email,
+                'phone' => $faker->phoneNumber,
+                'job_title' => $faker->jobTitle,
+                'office_id' => $office->id,
+            ]);
+        }
+               }
 
 
-        $db->getConnection()->statement("INSERT INTO `companies` VALUES
-    (1,'Stack Exchange','0601010101','stack@exchange.com','https://stackexchange.com/','https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Verisure_information_technology_department_at_Ch%C3%A2tenay-Malabry_-_2019-01-10.jpg/1920px-Verisure_information_technology_department_at_Ch%C3%A2tenay-Malabry_-_2019-01-10.jpg', now(), now(), null),
-    (2,'Google','0602020202','contact@google.com','https://www.google.com','https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Google_office_%284135991953%29.jpg/800px-Google_office_%284135991953%29.jpg?20190722090506',now(), now(), null)
-        ");
+        }
 
-        $db->getConnection()->statement("INSERT INTO `offices` VALUES
-    (1,'Bureau de Nancy','1 rue Stanistlas','Nancy','54000','France','nancy@stackexchange.com',NULL,1, now(), now()),
-    (2,'Burea de Vandoeuvre','46 avenue Jeanne d\'Arc','Vandoeuvre','54500','France',NULL,NULL,1, now(), now()),
-    (3,'Siege sociale','2 rue de la primatiale','Paris','75000','France',NULL,NULL,2, now(), now()),
-    (4,'Bureau Berlinois','192 avenue central','Berlin','12277','Allemagne',NULL,NULL,2, now(), now())
-        ");
 
-        $db->getConnection()->statement("INSERT INTO `employees` VALUES
-     (1,'Camille','La Chenille',1,'camille.la@chenille.com',NULL,'Ingénieur', now(), now()),
-     (2,'Albert','Mudhat',2,'albert.mudhat@aqume.net',NULL,'Superviseur', now(), now()),
-     (3,'Sylvie','Tesse',3,'sylive.tesse@factice.local',NULL,'PDG', now(), now()),
-     (4,'John','Doe',4,'john.doe@generique.org',NULL,'Testeur', now(), now()),
-     (5,'Jean','Bon',1,'jean@test.com',NULL,'Developpeur', now(), now()),
-     (6,'Anais','Dufour',2,'anais@aqume.net',NULL,'DBA', now(), now()),
-     (7,'Sylvain','Poirson',3,'sylvain@factice.local',NULL,'Administrateur réseau', now(), now()),
-     (8,'Telma','Thiriet',4,'telma@generique.org',NULL,'Juriste', now(), now())
-        ");
+        
 
-        $db->getConnection()->statement("update companies set head_office_id = 1 where id = 1;");
-        $db->getConnection()->statement("update companies set head_office_id = 3 where id = 2;");
+    //     $db->getConnection()->statement("INSERT INTO `companies` VALUES
+    // (1,'Stack Exchange','0601010101','stack@exchange.com','https://stackexchange.com/','https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Verisure_information_technology_department_at_Ch%C3%A2tenay-Malabry_-_2019-01-10.jpg/1920px-Verisure_information_technology_department_at_Ch%C3%A2tenay-Malabry_-_2019-01-10.jpg', now(), now(), null),
+    // (2,'Google','0602020202','contact@google.com','https://www.google.com','https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Google_office_%284135991953%29.jpg/800px-Google_office_%284135991953%29.jpg?20190722090506',now(), now(), null)
+    //     ");
+
+    //     $db->getConnection()->statement("INSERT INTO `offices` VALUES
+    // (1,'Bureau de Nancy','1 rue Stanistlas','Nancy','54000','France','nancy@stackexchange.com',NULL,1, now(), now()),
+    // (2,'Burea de Vandoeuvre','46 avenue Jeanne d\'Arc','Vandoeuvre','54500','France',NULL,NULL,1, now(), now()),
+    // (3,'Siege sociale','2 rue de la primatiale','Paris','75000','France',NULL,NULL,2, now(), now()),
+    // (4,'Bureau Berlinois','192 avenue central','Berlin','12277','Allemagne',NULL,NULL,2, now(), now())
+    //     ");
+
+    //     $db->getConnection()->statement("INSERT INTO `employees` VALUES
+    //  (1,'Camille','La Chenille',1,'camille.la@chenille.com',NULL,'Ingénieur', now(), now()),
+    //  (2,'Albert','Mudhat',2,'albert.mudhat@aqume.net',NULL,'Superviseur', now(), now()),
+    //  (3,'Sylvie','Tesse',3,'sylive.tesse@factice.local',NULL,'PDG', now(), now()),
+    //  (4,'John','Doe',4,'john.doe@generique.org',NULL,'Testeur', now(), now()),
+    //  (5,'Jean','Bon',1,'jean@test.com',NULL,'Developpeur', now(), now()),
+    //  (6,'Anais','Dufour',2,'anais@aqume.net',NULL,'DBA', now(), now()),
+    //  (7,'Sylvain','Poirson',3,'sylvain@factice.local',NULL,'Administrateur réseau', now(), now()),
+    //  (8,'Telma','Thiriet',4,'telma@generique.org',NULL,'Juriste', now(), now())
+    //     ");
+
+        // $db->getConnection()->statement("update companies set head_office_id = 1 where id = 1;");
+        // $db->getConnection()->statement("update companies set head_office_id = 3 where id = 2;");
 
         $output->writeln('Database created successfully!');
         return 0;
